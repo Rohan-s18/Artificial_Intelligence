4f99dd5f221388777d958be6fee6a374cc4a1402
Rohan Singh <68778986+Rohan-s18@users.noreply.github.com>
2022-10-06 10:31:52 -0400

    Adding Route Finder
    Adding Extra Credit part of the assignment, Implemented Route Finder as A* application.

--- CSDS_391/P1/src/RouteFinder.java
+++ CSDS_391/P1/src/RouteFinder.java
@@ -1,13 +1,32 @@
-
 import java.util.*;;
 
+/* Author: Rohan Singh
+ * Extra Credit
+ * Route Finder in a Graph which uses the A* algorithm and uses Euclidean distance between 2 points as the heuristic.
+ * This map is a directional weighted graph
+ * */
+
+
 public class RouteFinder {
+	
+	 /* That data is represented by multiple Arrays (name, position and the edges)
+	 * */
+	
+	
+	//Array of Coordinates of the vertices of the Graph
     private Coordinate[] position;
+    
+    //Array of the names of the Vertices of the Graph
     private String[] names;
+    
+    //2D Matrix represents the connections in the Graph
     private double[][] matrix;
+    
+    //Size counter for the Graph
     private int size;
     private int numVertices;
 
+    //Class representing the x,y coordinates of the vertices
     private class Coordinate{
         double x;
         double y;
@@ -20,6 +39,7 @@ public class RouteFinder {
         }
     }
     
+    //Constructor for the Graph
     public RouteFinder(){
         size = 100;
         names = new String[size];
@@ -28,24 +48,39 @@ public class RouteFinder {
         numVertices = 0;
     }
 
+    //Method to add a Vertex to the Graph
     public boolean addVertex(String name, int x, int y){
+    	//Checking if the Vertex exists already
         int index = indexOf(name);
-        if(index!=-1)return false;
+        if(index!=-1)
+        	return false;
+        
+        //Adding the data into their respective arrays
         names[numVertices] = name;
         position[numVertices] = new Coordinate(x, y);
         numVertices++;
         return true;
     }
     
+    //Method to add an edge between 2 vertices in a graph
     public boolean addEdge(String from, String to, double cost){
         int fromIndex = indexOf(from);
         int toIndex = indexOf(to);
-        if(fromIndex == -1 || toIndex == -1) return false;
-        if(matrix[fromIndex][toIndex] != 0) return false;
+        
+        //Checking if the vertices exist or not
+        if(fromIndex == -1 || toIndex == -1) 
+        	return false;
+        
+        //Checking if an edge exists or not
+        if(matrix[fromIndex][toIndex] != 0) 
+        	return false;
+        
+        //Adding the edge cost to the edges matrix
         matrix[fromIndex][toIndex] = cost;
         return true;
     }
 
+    //Method to help print the graph
     public void printGraph(){
         int neighborNum = 0;
         int j = 0;
@@ -62,8 +97,10 @@ public class RouteFinder {
         }
     }
 
+    //Method to find the shortest path using A* algorithm
     public void aStarShortestPath(String source, String destination){
 
+    	//Finding the index of the source and destination vertices
         int sourceIndex = indexOf(source);
         int destinationIndex = indexOf(destination);
         if(sourceIndex == -1 || destinationIndex == -1){
@@ -72,33 +109,42 @@ public class RouteFinder {
         }
             
 
-        //Array to store function values for the search heurestic
+        //Array to store function values for the search heuristic
         double[] h = new double[numVertices];
 
         //Array to store the values of the path cost
         double[] g = new double[numVertices];
 
-        //Array to store the function values
-        //double[] f = new double[numVertices];
-
         int numFinalized = 0;
+        
+        //Array to represent the finalized vertices (vertex at index 'i' will be finalized if finalized[i] is true)
         boolean[] finalized = new boolean[numVertices];
+        
+        //Array to store the parent indices of each vertex, this will help us in backtracking
         int[] parents = new int[numVertices];
 
+        //Initialization
         for(int i = 0; i < numVertices; i++){
+        	//Cost will be set to infinity
             g[i] = Integer.MAX_VALUE;
-            //f[i] = Integer.MAX_VALUE;
+            //The parents are unknown
             parents[i] = 0;
+            //Calculating the heuristic values for each of the states using a helper method
             h[i] = heurestic(i, destinationIndex);
         }
 
+        /*	Priority Queue to store the Node connections
+         *  The NodeConnection class implements the Comparable interface (required for PQ)
+         *  The NodeConnection class just holds fields for the index of the node and its current evaluation function value
+         */
         PriorityQueue<NodeConnection> pq = new PriorityQueue<NodeConnection>();
         pq.add(new NodeConnection(sourceIndex, 0));
-        parents[sourceIndex] = -1;
+        parents[sourceIndex] = -1;						//Parent of source is set to be -1
         g[sourceIndex] = 0;
-       // f[sourceIndex] = h[sourceIndex];
 
+        //Looping while the destination index is not finalized
         while(!finalized[destinationIndex] && numFinalized < numVertices){
+        	//Polling the node with lowest f-value
             int curr = pq.remove().index;
 
             if(finalized[curr]){
@@ -107,12 +153,17 @@ public class RouteFinder {
 
             System.out.printf("Node %s has been finalized\n",names[curr]);
 
+            //Finalizing the node
             finalized[curr] = true;
             numFinalized++;
 
+            //Processing the children of the finalized node
             for(int i = 0; i < numVertices; i++){
+            	//If the child has already not been finalized
                 if(matrix[curr][i] != 0 && !finalized[i]){
+                	//Calculating the new cost
                     double tempCost = g[curr] + matrix[curr][i];
+                    //If the new cost is less, then we will update it and add it to the PQ
                     if(tempCost < g[i]){
                         g[i] = tempCost;
                         pq.add(new NodeConnection(i, tempCost + h[i]));
@@ -123,6 +174,7 @@ public class RouteFinder {
 
         }
 
+        //Backtracking to get the path (using stack to reverse it)
         Stack<String> pathList = new Stack<String>();
         int curr = destinationIndex;
         do{
@@ -130,6 +182,7 @@ public class RouteFinder {
             curr = parents[curr];
         } while(curr != -1);
         
+        //This part is their to visualize the path
         String str = "";
         while(!pathList.isEmpty())
             str += pathList.pop() + " -> ";
@@ -140,7 +193,7 @@ public class RouteFinder {
 
     }
 
-
+    //Helper method to find the index of the vertex
     private int indexOf(String name){
         for(int i = 0; i < numVertices; i++){
             if(names[i].equals(name))
@@ -149,12 +202,14 @@ public class RouteFinder {
         return -1;
     }
 
+    //Helper method to calculate the heuristic
     private double heurestic(int curr, int destination){
         double xDiff = position[curr].x - position[destination].x;
         double yDiff = position[curr].y - position[destination].y;
         return Math.sqrt((Math.pow(xDiff, 2))+(Math.pow(yDiff, 2)));
     }
 
+    //Node connection class to store the index and evaluation function value of each node
     private class NodeConnection implements Comparable<NodeConnection>{
         int index;
         double functionValue;
