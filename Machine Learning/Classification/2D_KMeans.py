d43e4d0ae38ae7dbae7c854cabbdbc2e90995597
Rohan Singh <68778986+Rohan-s18@users.noreply.github.com>
2022-11-15 13:42:40 -0500

    Module for KMeans in 2 Dimensions

--- Machine Learning/Classification/2D_KMeans.py
+++ Machine Learning/Classification/2D_KMeans.py
@@ -1,14 +1,10 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Thu Nov  3 10:20:04 2022
+Created on Tue Nov 15 13:37:15 2022
 
 @author: rohansingh
 """
-
-# Source code for K-means clustering
-
-#Getting imports
 
 import numpy as np
 import pandas as pd
@@ -17,23 +13,7 @@ import matplotlib.pyplot as mtp
 #this is used to get the iris data
 df = pd.read_csv("/Users/rohansingh/Documents/CSDS 391/AI_Code/Machine Learning/Classification/irisdata.csv")
 
-def plotIris():
-    X = df.data[:, 3:]
-    y = df.target
-    mtp.scatter(X[:, 0],X[:, 1],c=y)
-    mtp.xlabel("Petal Length")
-    mtp.ylabel("Petal Width")
-    mtp.show()
-    
-def eucDist(x1, x2, y1, y2):
-    return np.sqrt( (np.power((x2-x1),2)) + (np.power((y2-y1),2)) )
-
-def objFunction(mu, pos1, pos2):
-    return 0
-
-
-
-class KMeans:
+class KMeans_2D:
     
     def __init__(self, k=3, max_iter=100,df=pd.read_csv("/Users/rohansingh/Documents/CSDS 391/AI_Code/Machine Learning/Classification/irisdata.csv")):
         self.k = k
@@ -44,27 +24,19 @@ class KMeans:
         dist_sum = 0
         dist_sum += np.power((point[0]-centroid[0]),2)
         dist_sum += np.power((point[1]-centroid[1]),2)
-        dist_sum += np.power((point[2]-centroid[2]),2)
-        dist_sum += np.power((point[3]-centroid[3]),2)
         return np.sqrt(dist_sum)
     
     #Pre-processing the data for convenience
     def getPoints(self):
-        s_len = self.df["sepal_length"]
-        s_wid = self.df["sepal_width"]
         p_len = self.df["petal_length"]
         p_wid = self.df["petal_width"]
         
-        s_len = s_len.to_numpy()
-        s_wid = s_wid.to_numpy()
         p_len = p_len.to_numpy()
         p_wid = p_wid.to_numpy()
         
         points = []
-        for i in range(0,len(s_len),1):
+        for i in range(0,len(p_len),1):
             row = []
-            row.append(s_len[i])
-            row.append(s_wid[i])
             row.append(p_len[i])
             row.append(p_wid[i])
             points.append(row)
@@ -94,19 +66,13 @@ class KMeans:
         new_centroids = []
         for i in range(0,self.k,1):
             tempclass = classes[i]
-            s_len_sum = 0
-            s_wid_sum = 0
             p_len_sum = 0
             p_wid_sum = 0
             for j in range(0,len(tempclass),1):
                 point = tempclass[j]
-                s_len_sum += point[0]
-                s_wid_sum += point[1]
-                p_len_sum += point[2]
-                p_wid_sum += point[3]
+                p_len_sum += point[0]
+                p_wid_sum += point[1]
             cl = []
-            cl.append(s_len_sum/len(tempclass))
-            cl.append(s_wid_sum/len(tempclass))
             cl.append(p_len_sum/len(tempclass))
             cl.append(p_wid_sum/len(tempclass))
             new_centroids.append(cl)
@@ -139,7 +105,7 @@ class KMeans:
             for j in range(0,self.k,1):
                 a = centroids[j]
                 b = new_centroids[j]
-                for k in range(0,4,1):
+                for k in range(0,2,1):
                     c = a[k]
                     d = b[k]
                     if(abs((c-d)/d) > 0.0001):
@@ -152,12 +118,6 @@ class KMeans:
             
         
         return classes
-        
-        
-    
-    
-    
-    
     
     
     
