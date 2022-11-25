effa8fdba57a2f29629fd24ff623938dcdf6736c
Rohan Singh <68778986+Rohan-s18@users.noreply.github.com>
2022-11-25 12:13:58 -0500

    Updated main method and added comments

--- Machine Learning/Classification/Classification Using Neural Networks/Linear Decision Boundaries/Sigmoid_Classifier.py
+++ Machine Learning/Classification/Classification Using Neural Networks/Linear Decision Boundaries/Sigmoid_Classifier.py
@@ -12,6 +12,8 @@ import plotly.express as px
 import random
 import plotly.io as pio
 import plotly.graph_objects as go
+from scipy.interpolate import griddata
+import matplotlib.pyplot as plt
 
 pio.renderers.default = 'browser'
 
@@ -44,6 +46,7 @@ def predict(X,weights,bias):
     return np.array(pred_class)
 
 #%%
+
 #This method will count the number of misclassifications
 def misclassifiction_count(test, predicted_vals):
     #Counter variable
@@ -57,7 +60,84 @@ def misclassifiction_count(test, predicted_vals):
     return ct
 
 #%%
+
+#This method will plot the output from the neural network over the given values from the dataset
+def plot_Results(df,_class):
+    #Plotting the scatterplot using plotly
+    fig = px.scatter(df, x="petal_length",y="petal_width",color=_class)
+    fig.show()
+
+
+#%%
+
 #This method will get the linear decision boundary
+def get_Linear_db(df,w,b,_class):
+    #Plotting the decision boundary
+    
+    #Getting the inctercept and the slope
+    c = -(b/w[1])
+    m = -(w[0]/w[1])
+    
+    #Making the arrays for the x and y axes
+    xd = np.array([2.5,7])
+    yd = m*xd + c
+    
+    fig_1 = go.Figure()
+    fig_1.add_trace(
+                go.Scatter(x=df["petal_length"], y=df["petal_width"],
+                mode='markers',
+                name='points',
+                marker = {'color':_class}
+                ))
+    fig_1.add_trace(go.Scatter(x=xd, y=yd,
+                mode='lines',
+                name='decision boundary'))
+    fig_1.show()
+
+
+
+#%%
+
+#This function will plot the output of the neural network over all input space
+def plot_over_space(weights, bias):
+    #Creating a dataframe of all of the input and output points 
+    z = []
+    #x = np.linspace(0.0,10.0,100)
+    #y = np.linspace(0.0,10.0,100)
+    x = []
+    y = []
+    for i in range(0,100,1):
+        for j in range(0,100,1):
+            x.append(i)
+            y.append(j)
+            arr = np.array([i,j])
+            temp = np.dot(arr, weights) + bias
+            temp = sigmoid(temp)
+            z.append(temp)
+    
+    #z = np.array(z)
+    temp_df = pd.DataFrame(list(zip(x, y, z)),columns =['x', 'y','z'])
+    
+    x1 = np.linspace(temp_df['x'].min(), temp_df['x'].max(), len(temp_df['x'].unique()))
+    y1 = np.linspace(temp_df['y'].min(), temp_df['y'].max(), len(temp_df['y'].unique()))
+
+    x2, y2 = np.meshgrid(x1, y1)
+
+    z2 = griddata((temp_df['x'], temp_df['y']), temp_df['z'], (x2, y2), method='cubic')
+
+    # Ready to plot
+    fig = plt.figure()
+    ax = fig.gca(projection='3d')
+    surf = ax.plot_surface(x2, y2, z2, rstride=1, cstride=1, linewidth=0, antialiased=False)
+    ax.set_zlim(0, 1.01)
+
+    #Setting for the plot
+    fig.colorbar(surf, aspect=5)
+    plt.title('Output of neural network over all input space')
+    plt.xlabel('Petal Length')
+    plt.ylabel('Petal Width')
+
+    plt.show()
 
 
 #%%
@@ -87,6 +167,8 @@ def main():
             temp.append(1)
     species = np.array(temp)
     
+    #End of data preprocessing
+    
     #Weight vector and bias scalars
     w = np.array([-0.05,0.51])
     b = -0.6
@@ -94,35 +176,20 @@ def main():
     #Predicting based on input weights and bias
     _class = predict(vals,w,b)
     
-    error = misclassifiction_count(species, _class)
-    print("The error with the current weights and biases is: ",error)
+    #error = misclassifiction_count(species, _class)
+    #print("The error with the current weights and biases is: ",error)
     
-    #Plotting the scatterplot using plotly
-    fig = px.scatter(df, x="petal_length",y="petal_width",color=_class)
-    fig.show()
+    #Calling the method that will plot the classification of the neural network
+    plot_Results(df, _class)
+    
+    #Calling the function that will plot the linear decision boundary
+    get_Linear_db(df, w, b, _class)
+    
+    #Calling the function that will plot the results of the neural network over all input space
+    plot_over_space(w, b)
     
     
-    #Plotting the decision boundary
     
-    #Getting the inctercept and the slope
-    c = -(b/w[1])
-    m = -(w[0]/w[1])
-    
-    #Making the arrays for the x and y axes
-    xd = np.array([2.5,7])
-    yd = m*xd + c
-    
-    fig_1 = go.Figure()
-    fig_1.add_trace(
-                go.Scatter(x=df["petal_length"], y=df["petal_width"],
-                mode='markers',
-                name='points',
-                marker = {'color':_class}
-                ))
-    fig_1.add_trace(go.Scatter(x=xd, y=yd,
-                mode='lines',
-                name='decision boundary'))
-    fig_1.show()
     
     
     
@@ -135,7 +202,6 @@ if __name__ == "__main__":
     main()
     
 #%%
-
 
 
 
