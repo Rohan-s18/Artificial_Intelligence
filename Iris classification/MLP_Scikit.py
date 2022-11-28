d60bb8fa2ab3a9acc42bab80efd94cd06c869728
Rohan Singh <68778986+Rohan-s18@users.noreply.github.com>
2022-11-28 15:56:58 -0500

    Multi Layered Perceptron Demo using Sci-kit learn

---
+++ Iris classification/MLP_Scikit.py
@@ -0,0 +1,57 @@
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.neural_network import MLPClassifier
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import classification_report
+
+#Using Sci-kit learns multi layer perceptron on the iris dataset for classification
+
+def run():
+    #print("Hello World!")
+    iris = load_iris()
+
+    #Splitting the data into different datasets
+    datasets = train_test_split(iris.data, iris.target,
+                            test_size=0.2)
+
+    train_data, test_data, train_labels, test_labels = datasets
+
+    scaler = StandardScaler()
+
+    # Fititng the train data
+    scaler.fit(train_data)
+
+    # scaling the train data
+    train_data = scaler.transform(train_data)
+    test_data = scaler.transform(test_data)
+
+    #Training the model
+    mlp = MLPClassifier(hidden_layer_sizes=(10, 5), max_iter=1000)
+
+    #Fiiting the training model
+    mlp.fit(train_data, train_labels)
+
+    #Running the prediction function on the training data
+    predictions_train = mlp.predict(train_data)
+    
+    #Running the prediction function on the testing data
+    predictions_test = mlp.predict(test_data)
+
+    """
+    print(accuracy_score(predictions_train, train_labels))
+    print(accuracy_score(predictions_test, test_labels))
+    print(classification_report(predictions_test, test_labels))
+    """
+
+    return predictions_test, predictions_train
+
+def main():
+    iris = load_iris()
+    print(type(iris.data))
+    test, train = run()
+    print(test)
+    print(train)
+
+if __name__ == "__main__":
+    main()
