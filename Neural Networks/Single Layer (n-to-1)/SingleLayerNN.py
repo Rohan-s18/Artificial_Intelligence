Fixed Gradient Bug

Neural Networks/Single Layer (n-to-1)/SingleLayerNN.py
@@ -70,6 +70,17 @@ class NeuralNetwork:
         #Getting the temporary predictions for the given set of weights
         y = self.get_NN_Output(weights=weights)
 
+        bias_sum = 0
+        #Iterating through all of the data/target points
+        for j in range(0,len(self.trainset),1):
+            #Finding the difference between the prediction and the target
+            temp = y[j] - self.targetset[j]
+
+            #Adding the difference to the bias sum
+            bias_sum += temp
+
+        gradient.append(bias_sum)
+
         #Iterating through all of the 'n' parameter dimensions
         for i in range(0,self.n,1):
 
@@ -89,6 +100,8 @@ class NeuralNetwork:
             #Appending the gradient sum for the i-th feature to the gradient vector
             grad_sum /= len(self.trainset)
             gradient.append(grad_sum)
+
+        
 
         return np.array(gradient)
 
@@ -117,7 +130,7 @@ class NeuralNetwork:
             summed_grad = self.get_summed_gradient(w)
             
             #Updating the weights for the next iteration
-            w -= epsilon*summed_grad
+            w -= (epsilon*summed_grad)
 
         self.trained_weight = w
         #Returning the optimal weights 
@@ -205,7 +218,7 @@ def main():
     DemoNN = NeuralNetwork(dataset=dataset,targets=target,n=4)
     #out = DemoNN.get_NN_Output(np.array([1,1,1,1,1]))
     DemoNN.train(0.001,500,10000)
-    out = DemoNN.predict()
+    out = DemoNN.predict(dataset)
     print(out)
 
 