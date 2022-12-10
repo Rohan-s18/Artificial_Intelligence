dfa992c8b0d99b1b6401af9b5a9080ce28098ad0
Rohan Singh <rohan.b.singh54@gmail.com>
2022-12-10 02:38:09 -0500

    Added training and predict functionality to the NN

--- Neural Networks/Single Layer (n-to-1)/SingleLayerNN.py
+++ Neural Networks/Single Layer (n-to-1)/SingleLayerNN.py
@@ -22,7 +22,7 @@ class NeuralNetwork:
     
 
     #This function will give you the output of the neural network given a weight vector
-    def getNNOutput(self, weights):
+    def get_NN_Output(self, weights):
 
         #This vector will contain the output of the neural network
         output = []
@@ -41,13 +41,105 @@ class NeuralNetwork:
         return np.array(output)
 
 
+    #This function will find the total squared error for the Neural Network from a given set of weights 
+    def get_TSE(self,weights):
 
-    #This will be used to train the Neural Network using gradient descent, using the input step-size and stopping point
-    def train(self, epsilon, maxerr):
+        #Predicting thee output of the neural network for the given weights
+        y = self.get_NN_Output(weights=weights)
+
+        #This variable will store the total squared error for the given output
+        tse = 0.0
+
+        #Iterating through all of the data points
+        for i in range(0,len(y),1):
+            #Adding the squared differences
+            tse += ((y[i] - self.targetset[i])**2)
         
+        #Dividing the sum by 2
+        tse /=2 
+
+        return tse
+
+    #This function will calculate the summed gradient that will be used in the learning rule
+    def get_summed_gradient(self,weights):
+        
+        #This list will hold the summed gradient for all of the 'n' parameters
+        gradient = []
+
+        #Getting the temporary predictions for the given set of weights
+        y = self.get_NN_Output(weights=weights)
+
+        #Iterating through all of the 'n' parameter dimensions
+        for i in range(0,len(weights),1):
+
+            #Calculating the gradient sum for the parameter
+            grad_sum = 0
+            
+            #Iterating through all of the data/target points
+            for j in range(0,len(self.trainset),1):
+                #Finding the difference between the prediction and the target
+                temp = y[j] - self.targetset[j]
+
+                #Multiplying the difference with the datapoint's i-th component
+                temp *= self.trainset[j][i]
+
+                grad_sum += temp
+
+            #Appending the gradient sum for the i-th feature to the gradient vector
+            grad_sum /= len(self.trainset)
+            gradient.append(grad_sum)
+
+        return np.array(gradient)
 
 
-        print("Lmao")
+    #This will be used to train the Neural Network using gradient descent, using the input step-size and stopping point and a maximum iteration count
+    def train(self, epsilon, maxerr, maxiter):
+        
+        #Setting the intial weights to 0 for all of the n-parameters (and the bias)
+        w = np.zeros(len(self.trainset[0])+ 1)
+
+        #Iterating within the maximum iteration limit
+        for i in range (0,maxiter,1):
+
+            #Getting the predicted values given the current weights
+            y = self.get_NN_Output(w)
+
+            #Geting the total squared error for the temporary output
+            tse = self.get_TSE(y)
+
+            #Checking if the mean-squared error has minimized to the threshold
+            mse = tse/(len(self.trainset))
+            if(mse < maxerr):
+                break
+
+            #Getting the summed gradient
+            summed_grad = self.get_summed_gradient(w)
+            
+            #Updating the weights for the next iteration
+            w -= epsilon*summed_grad
+
+        self.trained_weight = w
+        #Returning the optimal weights 
+        return w
+
+    #This will be used as the predict function for the neural-network after the training on the test-set
+    def predict(self, test_set):
+        
+        #This vector will contain the output of the neural network
+        output = []
+
+        #Getting the output for all of the datapoints within the test set
+        for i in range (0,len(test_set),1):
+
+            #This will be added for the bias-term
+            a = np.array([1]) 
+            a = np.concatenate((a,test_set[i]),axis=0)
+
+            #Appending the output for the point (inner-product with the optimum weights) into the output list
+            output.append(np.dot(a,self.trained_weight))
+
+        #Converting the output into a numpy array
+        return np.array(output)
 
     def printHello(self):
         print("Hello World!")
@@ -58,11 +150,11 @@ class NeuralNetwork:
 This cell contains a helper function to pre-process the data for the demonstration in the main method
 """
 
-def getData():
+def get_data():
     dataset = []
     target = []
 
-    filepath = ""
+    filepath = "/Users/rohansingh/Documents/CSDS 391/AI_Code/Machine Learning/Classification/irisdata.csv"
 
     #Reading the csv from the filepath
     df = pd.read_csv(filepath)
@@ -106,11 +198,14 @@ This cell contains the main method to show the demonstration of the code
 def main():
 
     #Fetching the data using the helper function in the cell above
-    dataset, target = getData()
+    dataset, target = get_data()
 
     #Instantiating the Neural Network Object
     DemoNN = NeuralNetwork(dataset=dataset,targets=target)
-    DemoNN.printHello()
+    #out = DemoNN.get_NN_Output(np.array([1,1,1,1,1]))
+    DemoNN.train(0.001,500,10000)
+    out = DemoNN.predict()
+    print(out)
 
 
 
