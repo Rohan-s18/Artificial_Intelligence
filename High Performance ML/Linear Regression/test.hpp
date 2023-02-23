e9cf05dc8fe294c07f4380b3d59eada75c09aefb
Rohan Singh <68778986+Rohan-s18@users.noreply.github.com>
2023-02-23 18:54:11 -0500

    Updated testing header

--- High Performance ML/Linear Regression/test.hpp
+++ High Performance ML/Linear Regression/test.hpp
@@ -4,3 +4,10 @@
 void single_variable_demo();
 
 void single_variable_test();
+
+void print_array(double arr[], int n){
+    std::cout<<"[";
+    for(int i = 0; i < n; i++)
+        std::cout<<arr[i]<<" ";
+    std::cout<<"]\n";
+};
