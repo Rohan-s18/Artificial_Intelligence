0719bf444ff29d7098420bdb9d9433c20c3555fc
Rohan Singh <rohan.b.singh54@gmail.com>
2023-02-27 13:44:03 -0500

    Added objective function

--- High Performance ML/KMeans/KMeans.cpp
+++ High Performance ML/KMeans/KMeans.cpp
@@ -6,6 +6,7 @@
 #include <iostream>
 #include <cmath>
 #include "Testing.hpp"
+#include <float.h>
 
 
 //  Class for KMeans
@@ -20,7 +21,7 @@ class KMeans{
     int k;
     double** dataset;
     int* cluster;
-    double* centroids;
+    double** centroids;
     int* cluster_len;
 
 
@@ -37,11 +38,55 @@ class KMeans{
     //function for the objective function of KMeans algorithm
     void objective_function(){
 
+        //Iterating though all of the datapoints
+        for(int i = 0; i < this->data_len; i++){
+            int cl = -1;
+            double dist = DBL_MAX;
+            double temp = 0;
+
+            //Iterating through all of the centroids
+            for(int j = 0; j < this->k; j++){
+                temp = this->euc_dist(this->dataset[i],this->centroids[j]);
+
+                if(temp < dist){
+                    dist = temp;
+                    cl = j;
+                }
+            }
+
+            //Classifying the cluster for 'i' as cl
+            this->cluster[i] = cl;
+
+        }
+
     }
 
 
     //function to update the centroids for each cluster
     void update_centroids(){
+
+        //Creating matrix for new centroids
+        double** updated_centroids = new double*[this->k];
+        for(int i = 0; i < this->k; i++)
+            updated_centroids[i] = new double[this->dim];
+        
+
+
+        //Iterating through each data point
+        for(int i = 0; i < this->data_len; i++){
+
+            //Holds the sum for each dimension
+            double cluster_sum[this->dim];
+            
+            //Adding the 
+
+        }
+
+
+
+
+
+
 
     }
 
@@ -55,13 +100,26 @@ class KMeans{
         this->k = num_clusters;
         this->dataset = data;
         this->cluster = new int[data_length];
-        this->centroids = new double[num_clusters];
         this->cluster_len = new int[num_clusters];
+        this->centroids = new double*[num_clusters];
+        for(int i = 0; i < num_clusters; i++)
+            this->centroids[i] = new double[this->dim];
+
     }
 
     //Predict function which returns the cluster for each corressponding data point
     int* predict(){
 
+        return this->cluster;
+    }
+
+    void print_centroids(){
+        for(int i = 0; i < this->k; i++){
+            std::cout<<"[";
+            for(int j = 0; j < this->dim; j++)
+                std::cout<<this->centroids[i][j]<<" ";
+            std::cout<<"]\n";
+        }
     }
 
 
@@ -80,6 +138,10 @@ int main(){
 
 //  Implementation of demo
 void kmeans_demo(){
+
+    KMeans* demo = new KMeans(5, 100, 6, NULL);
+
+    demo->print_centroids();
 
 }
 
