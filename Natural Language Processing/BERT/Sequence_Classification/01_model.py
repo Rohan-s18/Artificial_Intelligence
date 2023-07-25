d06f096549a676b3a66bbf54bbba65754f030716
Rohan Singh <rohan.b.singh54@gmail.com>
2023-07-25 17:37:38 -0400

    Added Basic Model Code

---
+++ Natural Language Processing/BERT/Sequence_Classification/01_model.py
@@ -0,0 +1,59 @@
+"""
+Author: Rohan Singh
+Date: July 25, 2023
+This module contains code for a simple sequence classification model using DistilBERT
+"""
+
+#%%
+"""
+Librarires Used
+"""
+
+# EDA Libraries
+
+# Scikit-Learn Libraries
+
+# BERT Support Libraries
+
+# BERT Libraries
+
+#%%
+"""
+Function for making a dataset
+"""
+
+def make_dataset(df, text_title, sequence_title, split_size):
+    pass
+
+
+#%%
+"""
+Function to preprocess the Data
+"""
+
+def preprocess_function(examples):
+    pass
+
+
+#%%
+"""
+Function to tokenize the dataset
+"""
+
+def tokenize_dataset(dataset, tokenizer, stride):
+    pass
+
+
+#%%
+"""
+Main function to run the modek
+"""
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
+
+
+#%%
