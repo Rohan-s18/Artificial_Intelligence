3e6e2884a041ab190c89d54c0f9196b5e699e625
Rohan Singh <rohan.b.singh54@gmail.com>
2023-07-31 15:41:05 -0400

    Added Basic BERT Model

--- Natural Language Processing/BERT/Sequence_Classification/01_model.py
+++ Natural Language Processing/BERT/Sequence_Classification/01_model.py
@@ -45,6 +45,8 @@ STRIDE=32
 
 nlp = spacy.load('en_core_web_sm')
 
+metric = load_metric('accuracy')
+
 
 
 #%%
@@ -84,11 +86,81 @@ def tokenize_dataset(dataset, tokenizer):
 
 #%%
 """
-Main function to run the modek
+Function for the computation metric for the training loop
+"""
+def compute_metrics(eval_pred):
+    logits, labels = eval_pred
+    predictions = np.argmax(logits, axis=-1)
+    return metric.compute(predictions=predictions, references=labels)
+
+
+#%%
+"""
+Main function to run the model
 """
 
 def main():
-    pass
+    
+    # Obtaining the dataframe
+    filepath= ""
+    df = pd.read_csv(filepath)
+    demo_text_title = ""
+    demo_sequence_title = ""
+
+    # Creating the dataset
+    demo_dataset = make_dataset(df, sequence_title=demo_sequence_title, text_title=demo_text_title)
+
+    # Tokenizing the dataset
+    tokenizer = DistilBertTokenizerFast.from_pretrained('distilbert-base-uncased')
+    demo_dataset = tokenize_dataset(dataset=demo_dataset)
+    data_collator = DataCollatorWithPadding(tokenizer=tokenizer)
+
+    #Loading the model
+    id2label = {}
+    label2id = {}
+    num_labels = 0
+    sequence_clf_model = DistilBertForSequenceClassification.from_pretrained('distilbert-base-uncased',num_labels=num_labels,
+                                                                             id2label=id2label,label2id=label2id)
+    
+    #Setting up the training arguments
+    epochs = 20
+    output_dir = ""
+    #training_args = TrainingArguments(output_dir=output_dir, num_train_epochs=epochs, per_device_train_batch_size=32, per_gpu_eval_batch_size=32, load_best_model_at_end=True, warmup_steps = len(demo_dataset['train'])weight_decay=0.05, logging_steps=1, log_level='info', evaluation_strategy='epoch', save_strategy='epoch')
+    training_args = TrainingArguments(
+
+        output_dir=output_dir,
+        num_train_epochs=epochs,
+        per_device_train_batch_size=32,
+        per_gpu_eval_batch_size=32,
+        load_best_model_at_end=True,
+
+        warmup_steps=len(demo_dataset['train']),
+        weight_decay=0.05,
+
+        logging_steps=1,
+        log_level='info',
+        evaluation_strategy='epoch',
+        save_strategy='epoch'
+
+    )
+
+    # Setting up the trainer
+    trainer = Trainer(
+        model=sequence_clf_model,
+        args=training_args,
+        train_dataset=demo_dataset['train'],
+        eval_dataset=demo_dataset['test'],
+        compute_metrics=compute_metrics,
+        data_collator=data_collator
+    )
+
+    # Training the model
+    print("\nInitial:\n",trainer.evaluate(),"\n\n")
+    trainer.train()
+    print("Final:\n",trainer.evaluate(),"\n\n")
+
+
+
 
 if __name__ == "__main__":
     main()
