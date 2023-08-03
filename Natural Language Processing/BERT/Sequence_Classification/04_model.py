67645bca1a06110bc7f53ae63a9f7602a6e6fa9e
Rohan Singh <rohan.b.singh54@gmail.com>
2023-08-02 22:59:10 -0400

    pushing to github, end of session

--- Natural Language Processing/BERT/Sequence_Classification/04_model.py
+++ Natural Language Processing/BERT/Sequence_Classification/04_model.py
@@ -5,3 +5,43 @@ Sequence Classification Model using DistilBERT
 """
 
 # Imports
+
+# EDA Libraries
+import numpy as np
+import pandas as pd
+
+# Scikit-learn
+from sklearn.linear_model import LinearRegression, LogisticRegression
+from sklearn.model_selection import train_test_split
+
+# BERT Support Libraries
+#import tensorflow as tf
+import torch
+from datasets import Dataset, load_metric
+
+# BERT imports
+from transformers import TrainingArguments, Trainer, DistilBertTokenizerFast, DistilBertForSequenceClassification, DataCollatorWithPadding, pipeline
+
+
+# Data pre-process function
+def preprocess_function(examples):
+    pass
+
+
+# Main function
+def main():
+    
+    df = pd.read_csv("/Users/rohansingh/Desktop/df_main.csv")
+
+
+#%%
+
+df = pd.read_csv("/Users/rohansingh/Desktop/df_main.csv")
+
+#%%
+
+
+if __name__ == "__main__":
+    main()
+
+
