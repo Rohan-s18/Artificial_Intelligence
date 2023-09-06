2863c3a514960c554b7f0df809f8d578f6e0e2dd
Rohan Singh <rohan.b.singh54@gmail.com>
2023-09-06 14:21:32 -0400

    Added mps

--- PyTorch/gpu_setup.py
+++ PyTorch/gpu_setup.py
@@ -1,11 +1,15 @@
 import torch
 
+# defining the device
 mps_device = torch.device("mps")
 
 # Create a Tensor directly on the mps device
 x = torch.ones(5, device=mps_device)
-# Or
+
 x = torch.ones(5, device="mps")
 
 # Any operation happens on the GPU
 y = x * 2
+
+print(x.device)
+print(y.device)
